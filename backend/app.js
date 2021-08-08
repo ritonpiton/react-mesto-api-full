614259f3baaa31df4b6b194f3cbdef4f7a28e613
ritonpiton <rituhanych@yandex.ru>
2021-08-09 01:56:02 +0300

    mesto 15.1

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { login, createUser } = require('./controllers/users');
 const NotFoundError = require('./errors/NotFoundError');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 /* ---------- Helmet ---------- */
