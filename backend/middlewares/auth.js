d9b16ad24c58d18ae6725c45c60e3860273b3a02
ritonpiton <rituhanych@yandex.ru>
2021-08-09 02:53:25 +0300

    mesto 15.1

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Ошибка авторизации');
+  }
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+  try {
+    // попытаемся верифицировать токен
+    payload = jwt.verify(token, 'some-secret-key');
+  } catch (err) {
+    // отправим ошибку, если не получилось
+    throw new UnauthorizedError('Ошибка авторизации');
+  }
+  req.user = payload; // записываем пейлоуд в объект запроса
+  next(); // пропускаем запрос дальше
+};
