7a22edf21d21e136cd4cc3acb9528f5d0f576de6
ritonpiton <rituhanych@yandex.ru>
2021-08-09 22:36:01 +0300

    cors fix

--- frontend/src/utils/mestoAuth.js
+++ frontend/src/utils/mestoAuth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = '//mesto.frontend.domain.nomoredomains.rocks' || '//localhost:3001';
+export const BASE_URL = '//api.mesto.frontend.domain.nomoredomains.rocks' || '//localhost:3001';
 // export const BASE_URL = '//api.mesto.frontend.domain.nomoredomains.rocks';
 
 const checkResponse = (res) => {
